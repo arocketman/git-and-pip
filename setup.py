3a1e0e88dc44c31b0753529244f6df3e163959ac
Andrea Capuano <and.capuano@gmail.com>
2019-10-11 18:27:41 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="git-and-pip",
+    version="0.0.1",
+    author="arocketman",
+    author_email="and.capuano@gmail.com",
+    description="It's pip... with git.",
+    long_description=long_description,
+    url="https://github.com/arocketman/git-and-pip",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
